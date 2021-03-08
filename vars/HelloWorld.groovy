1e7d6dba35cb8a02061503621c16808cd51555b6
gagan4491 <50825088+gagan4491@users.noreply.github.com>
2021-03-08 17:17:58 +0530

    Update HelloWorld.groovy

--- vars/HelloWorld.groovy
+++ vars/HelloWorld.groovy
@@ -1,9 +1,3 @@
-import groovy.transform.Field
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
-
-
-
 def call() {
     println 'Hello World!'
 }
